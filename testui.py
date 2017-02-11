e6356fb4ece57b490a7061d48f094fa8a646730f
Jony <liu.z.jonathan@gmail.com>
2017-02-11 13:01:56 -0800

    asdfsfasdfsfdsf

--- testui.py
+++ testui.py
@@ -1,27 +1,33 @@
 from PySide.QtCore import *
 from PySide.QtGui import *
+from pubnub.enums import PNStatusCategory
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub, SubscribeListener
+from datetime import datetime
 import sys
+
+#from pntest import console_enter
 
 class Chat(QDialog):
     def __init__(self):
         super(Chat, self).__init__()
         
-        self.setWindowTitle("Developer Console")
-        self.desc = 'Chat Program'
+        self.setWindowTitle("Chat")
+        self.desc = 'Chat Program\n'
 
         self.usernameLabel = QLabel('Username')
         self.usernameEdit = QLineEdit()
-        self.usernameLayout = QHBoxLayout(self)
+        self.usernameLayout = QHBoxLayout()
         self.usernameLayout.addWidget(self.usernameLabel)
         self.usernameLayout.addWidget(self.usernameEdit)
 
-        self.prev_text = QTextEdit("<Bald Engineers Developer Console>")
+        self.prev_text = QTextEdit()
         self.prev_text.setText(self.desc)
         self.prev_text.setReadOnly(True)
         
         self.curr_text = QLineEdit()
         self.curr_text_btn = QPushButton("Enter")
-        self.curr_text_btn.clicked.connect(self.enter)
+        self.curr_text_btn.clicked.connect(self.console_enter)
         
         self.curr_text_layout = QHBoxLayout()
         self.curr_text_layout.addWidget(self.curr_text)
@@ -35,10 +41,53 @@ class Chat(QDialog):
         self.setLayout(self.console_form)
         self.show()
 
-    def enter(self):
-        pass
+    def console_enter(self):
+        
+        self.text = self.curr_text.displayText()
+        self.time = str(datetime.now()).split(".")[0]
+        self.username = str(self.usernameEdit.text())
+        self.message  = str(self.curr_text.text())
+
+        self.prev_text.setText(self.prev_text.toPlainText() + '\n' + self.message)
+        
+        pubnub.publish().channel('main_eh').message([self.username,self.time,self.message])\
+            .should_store(True).use_post(True).async(publish_callback)
+
+        self.curr_text.setText("")
+
+        result = my_listener.wait_for_message_on('main_eh')
+        self.hear_message(result.message)
+
+    def hear_message(self, message):
+        self.newMessageAll = message[1] + " " + message[0] + ": " + message[2] + "\n"
+        self.text = self.curr_text.displayText()
+        self.textNew = self.text + self.newMessageAll
+
+        self.prev_text.setText(self.textNew)
+
+
+
+def publish_callback(result, status):
+    pass
+
+
         
 if __name__ == '__main__':
+
+    #pubnub
+    pnconfig = PNConfiguration()
+    pnconfig.subscribe_key = "sub-c-d785fd74-f08e-11e6-9283-02ee2ddab7fe"
+    pnconfig.publish_key = "pub-c-afbd56e9-6341-4a07-b493-cbb3ebf25284"
+    pnconfig.ssl = False
+     
+    pubnub = PubNub(pnconfig)
+
+    my_listener = SubscribeListener()
+    pubnub.add_listener(my_listener)
+
+    pubnub.subscribe().channels('main_eh').execute()
+
+    
     #Main Program
     app = QApplication(sys.argv)
     main = Chat()
