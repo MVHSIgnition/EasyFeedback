f254bf3f50d4614feefad9f91ae27ef3d2825c49
nakulshn <nakulshn@gmail.com>
2017-02-11 18:44:24 -0800

    Create reader.js

---
+++ reader.js
@@ -0,0 +1,11 @@
+
+
+function doFunction(){
+  var question = document.getElementById('question');
+  var option1 = document.getElementById('option1');
+  var option2 = document.getElementById('option2');
+  var option3 = document.getElementById('option3');
+  var option4 = document.getElementById('option4');
+
+  var info = {"Question":question.value, "Option 1":option1.value,"Option2":option2.value, "Option3":option3.value, "Option4":option4.value}
+}
